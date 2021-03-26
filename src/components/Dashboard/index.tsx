d6933e248982213844f87febd0f7ca0654f7eac7
acristh <andre.cristiano1975@gmail.com>
2021-03-26 15:17:33 -0300

    setup:repair Dashboard component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,20 +1,14 @@
 import { Container } from "./styles";
 import { Summary } from "../Summary";
-<<<<<<< HEAD
-import { TransactionTable } from "../TransactionTable";
-=======
 import { TransactionsTable } from "../TransactionsTable";
->>>>>>> 2cc8a431bdd9fa27bdd0e2f68bc842d43cb5fd5e
+
 
 export function Dashboard() {
     return (
         <Container>
             <Summary />
-<<<<<<< HEAD
-    <TransactionTable />
-=======
             <TransactionsTable />
->>>>>>> 2cc8a431bdd9fa27bdd0e2f68bc842d43cb5fd5e
+            <TransactionsTable />
         </Container >
     );
 };
